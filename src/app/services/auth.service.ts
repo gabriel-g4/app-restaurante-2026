def53e588aa5faa858f6f6313b242a82feabf3f7
gabriel-g4 <gbgarcia642@gmail.com>
2026-05-16 10:56:25 -0300

    fix: fix push notification

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Auth, signInWithEmailAndPassword, signOut, authState, createUserWithEmailAndPassword, onAuthStateChanged,  } from '@angular/fire/auth';
+import { Firestore, doc, updateDoc } from '@angular/fire/firestore';
 
 @Injectable({
   providedIn: 'root',
@@ -9,7 +10,7 @@ export class AuthService {
   usuario$ = authState(this.auth);
   private rolActual: string = '';
 
-  constructor(private auth: Auth) { }
+  constructor(private auth: Auth, private firestore: Firestore) { }
 
   iniciarSesionConContrasenia(correo: string, contrasenia: string) {
     return signInWithEmailAndPassword(this.auth, correo, contrasenia);
@@ -19,7 +20,17 @@ export class AuthService {
     return createUserWithEmailAndPassword(this.auth, email, password);
   }
 
-  cerrarSesion() {
+  async cerrarSesion() {
+     const user = this.auth.currentUser;
+
+    if (user) {
+
+      const userRef = doc(this.firestore, 'usuarios', user.uid);
+
+      await updateDoc(userRef, {
+        push_token: ''
+      });
+    }
     this.rolActual = '';
     return signOut(this.auth);
   }
